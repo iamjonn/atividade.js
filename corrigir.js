a1412b3de63a65fafbd1cb2fd81d9a80f00ed8ed
iamjonn <jonata10.jn@gmail.com>
2023-09-17 20:03:37 -0300

    feito em funcao

--- corrigir.js
+++ corrigir.js
@@ -1,12 +1,14 @@
 function Corrigir(Q1,Q2,Q3,Q4,Q5){
   nota = 0
+  result = ''
   Q1 === "a" ? nota+=2:nota+=0;
   Q2 === "b" ? nota+=2:nota+=0;
   Q3 === "a" ? nota+=2:nota+=0;
   Q4 === "c" ? nota+=2:nota+=0;
   Q5 === "d" ? nota+=2:nota+=0;
-
-  return nota
+  nota < 7 ? result =`reprovado com nota ${nota}😪`:result =`aprovado com nota ${nota}😁`;
+  return result
 }
-
-console.log(Corrigir('a','b','b','b','b'))
+aluno = 'jon'
+result = Corrigir('a','b','b','b','b')
+console.log(`${aluno} foi ${result}`)
