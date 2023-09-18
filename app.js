c09ef4d66f1949b4fedc1ed85c566bbda2983811
iamjonn <jonata10.jn@gmail.com>
2023-09-18 02:58:02 -0300

    finalizado e testado

--- app.js
+++ app.js
@@ -1,13 +1,37 @@
-import Corrigir from './corrigir.js';
+import {Corrigir} from './corrigir.js';
 
 
 const peso = {q1:2, q2:2, q3:2, q4:2, q5:2}; // feito separadamente para possivel alteração de peso
-const gabarito = {q1:"a", q2:"b", q3:"a", q4:"c", q5:"d"}
+const gabarito = {q1:"A", q2:"b", q3:"A", q4:"c", q5:"D"}
 const prova = new Corrigir(gabarito, peso);
 
-prova.add({
+//TESTADO COM 1 ALUNO OU VARIOS, TERMINADO COM 5
+
+prova.compara({
   estudante: 'jon,',
-  gabarito: {q1:"a", q2:"b", q3:"b", q4:"b", q5:"b"},
+  gabarito: {q1:"a", q2:"B", q3:"b", q4:"b", q5:"b"},
 });
 
-console.log(Corrigir(prova))
+prova.compara({
+  estudante: 'alice,',
+  gabarito: {q1:"a", q2:"b", q3:"a", q4:"C", q5:"d"},
+});
+
+prova.compara({
+  estudante: 'joao,',
+  gabarito: {q1:"a", q2:"b", q3:"a", q4:"b", q5:"d"},
+});
+prova.compara({
+  estudante: 'julia,',
+  gabarito: {q1:"a", q2:"b", q3:"A", q4:"b", q5:"d"},
+});
+prova.compara({
+  estudante: 'ISA,',
+  gabarito: {q1:"B", q2:"b", q3:"c", q4:"b", q5:"D"},
+});
+
+console.log(`A media geral da turma foi: ${prova.media()}`);
+console.log(`A nota mais baixa foi: ${prova.baixa()}`);
+console.log(`A nota mais alta foi: ${prova.alta()}`);
+console.log(`As notas dos reprovados são: ${prova.reprovados()} e foram ${prova.reprovados().length} reprovados😢`);
+console.log(`As notas dos aprovados são: ${prova.aprovados()} e foram ${prova.aprovados().length} aprovados😁`);
--- corrigir.js
+++ corrigir.js
@@ -2,26 +2,40 @@ class Corrigir{
   constructor(gabarito, peso){
     this.peso = peso;
     this.gabarito = gabarito;
-    this.provas = [];
+    this.provas = []; //push linha 11 colocando todas as provas para comparaçao
   }
-  add(prova){
-    prova.grade = Object.chaveArray(prova.gabarito).calculo((total, j) => {
-      return total += (prova.gabarito[j]=== this.gabarito[j]? this.peso[j]:0)
+  compara(prova){
+    prova.array = Object.keys(prova.gabarito).reduce((total, j) => {
+      return total += (prova.gabarito[j].toLowerCase()=== this.gabarito[j].toLowerCase()? this.peso[j]:0)
     },0);  //colocado o '0' para retirar o error no caso de array vazio
     this.provas.push(prova);
   }
 
+  media(){
+    let sum = Object.values(this.provas).reduce((sum, j) => sum + j.array,0);
+    return sum /this.provas.length; //usado a variavel 'sum' para declarar que é um numero e prosseguir com a soma
+  }
+
+  baixa( j = 1){
+    let array = this.provas.map((gabarito) => gabarito.array);
+    array.sort((a, b) => a-b); return array.slice(0, j).join();
+  }
+
+  alta( j = 1){
+    let array = this.provas.map((gabarito) => gabarito.array);
+    array.sort((a, b) => a-b); return array.slice(-j).join();
+  }
+
+  reprovados(j = 7){
+    let array = this.provas.map((gabarito)=> gabarito.array);
+    array.sort().reverse(); //deixando em ordem decrescente
+    return array.filter((array)=> array < j)
+  }
+  
+  aprovados(j = 7){
+    let array = this.provas.map((gabarito)=> gabarito.array);
+    array.sort().reverse(); //deixando em ordem decrescente
+    return array.filter((array)=> array > j)
+  }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-export default {Corrigir}
+export{Corrigir}
